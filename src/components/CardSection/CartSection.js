6880a4b571cba8dc309fe2ace1d615b9dff688c6
SudiptaDeyAnik08 <sudiptadeysd06@gmail.com>
2023-04-28 14:31:08 +0600

    Profile Added

--- src/components/CardSection/CartSection.js
+++ src/components/CardSection/CartSection.js
@@ -1,9 +1,27 @@
 import React from "react";
 
+import "./CartSection.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHippo, } from "@fortawesome/free-solid-svg-icons";
+
+import profile_pic from "../../Assets/images/pro.jpg";
+
+
+
 const CartSection=()=>{
     return(
         <div>
-            <h1>Hi from Cart</h1>
+            <div className="profile">
+                <div className="image_div">
+                    <img src={profile_pic} alt="" />
+                </div>
+                <div className="profile_des">
+                    <h3>Sudipta Dey Anik</h3>
+                    <p>Dhaka,Bangladesh</p>
+                    {/* Icon add kora baki */}
+                </div>
+            </div>
         </div>
     )
 }
